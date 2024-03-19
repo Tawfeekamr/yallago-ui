292461b00f3f84b6b806bd308acbe3a433359b58
tawfeekamr <tawfeekamr91@gamil.com>
2024-03-19 22:37:08 +0300

    Add new config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+      fontFamily: {
+          'inter': ['Inter']
+      },
+    extend: {
+      colors: {
+         gray: {
+           lighter: "#E5E5E6",
+           light: "#E7E7E7",
+           soft: "#676879",
+           dark: "#808080",
+         },
+        black: {
+           soft: "#121212"
+        },
+        pink: {
+           soft: "#FF626A"
+        },
+        red: {
+           soft: "#ED4D55"
+        }
+      },
+      margin: {
+
+      }
+    },
   },
   plugins: [],
 }
